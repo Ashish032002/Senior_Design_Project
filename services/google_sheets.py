be8d0061c32ef16e4c301405a848337539f59a34
Ashish Singh <90508026+Ashish032002@users.noreply.github.com>
2025-04-16 17:49:17 +0530

    Update google_sheets.py

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -8,12 +8,23 @@ from typing import List, Any
 @st.cache_resource
 def get_sheets_service():
     """Cache Google Sheets service configuration"""
+    ##  try:
+    ##    creds = service_account.Credentials.from_service_account_file( # type: ignore
+    ##        os.getenv('GOOGLE_SHEETS_CREDENTIALS'), 
+    ##        scopes=['https://www.googleapis.com/auth/spreadsheets']
+    ##   )
+    ##    service: Any = build('sheets', 'v4', credentials=creds)
+    ##    return service
+    ##except Exception as e:
+    ##    raise e
+
     try:
-        creds = service_account.Credentials.from_service_account_file( # type: ignore
-            os.getenv('GOOGLE_SHEETS_CREDENTIALS'), 
-            scopes=['https://www.googleapis.com/auth/spreadsheets']
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"]
         )
-        service: Any = build('sheets', 'v4', credentials=creds)
-        return service
+        print("Credentials loaded")
+    
+        service = build('sheets', 'v4', credentials=credentials)
+        print("Google Sheets service built")
     except Exception as e:
-        raise e
+        st.error(f" Google Sheets failed: {e}")
